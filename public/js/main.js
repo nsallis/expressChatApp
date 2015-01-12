1f479bafd2dae234fd4e698c696a4f889db37d87
silvertale <n.sallis@hotmail.com>
2015-01-11 22:57:26 -0500

    test

--- public/js/main.js
+++ public/js/main.js
@@ -15,13 +15,13 @@ document.getElementById("send").onclick = function(){
 	document.getElementById('usertext').value = "";
 };
 
-function keyPress(e, form){
+/*function keyPress(e, form){
 	var key=e.keyCode || e.which;
 	if (key==13){
 	socket.emit('message sent', document.getElementById('usertext').value);
 	document.getElementById('usertext').value = "";
 	}
-};
+};*/
 
 
 socket.on('message', function(data) {
