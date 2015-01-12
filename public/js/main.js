d7e6d1e381702c130d994ae871ce6e38398a12b1
silvertale <n.sallis@hotmail.com>
2015-01-11 23:36:29 -0500

    test

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,10 @@ $('#reset').click(function() {
     socket.emit('reset');
 });
 
+document.getElementById("username").defaultValue = "User_" + Math.floor(Math.random() * 100) + 1;
+
 document.getElementById("send").onclick = function(){
-	socket.emit('message sent', document.getElementById('usertext').value);
+	socket.emit('message sent', "|" + document.getElementById("username").value + "|" + document.getElementById('usertext').value);//send the text with the username surrounded by "|"
 	document.getElementById('usertext').value = "";
 };
 
@@ -23,5 +25,9 @@ $("#chatform").submit(function() {
 
 
 socket.on('message', function(data) {
-    document.getElementById("conversation").innerHTML += "</br>" + data;
+    var start = data.indexOf('|') + 1;
+    var end = data.indexOf('|', start);
+    var user = data.substring(start, end)
+    var text = data.substring(end+1);
+    document.getElementById("conversation").innerHTML += "</br></br>" + user.bold() + "</br>" + text;
 });
