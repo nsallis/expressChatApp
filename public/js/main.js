7e065222d4f439794ef4086bb1f9ca97e122b8ba
silvertale <n.sallis@hotmail.com>
2015-01-11 22:40:54 -0500

    test

--- public/js/main.js
+++ public/js/main.js
@@ -12,13 +12,8 @@ $('#reset').click(function() {
 
 document.getElementById("send").onclick = function(){
 	socket.emit('message sent', document.getElementById('usertext').value);
+	document.getElementById('usertext').value = "";
 };
-
-$("#id_of_textbox").keyup(function(event){//enter pushed in textbox (input field)
-    if(event.keyCode == 13){
-        socket.emit('message sent', document.getElementById('usertext').value);
-    }
-});
 
 socket.on('message', function(data) {
     document.getElementById("conversation").innerHTML += </br> + data;
