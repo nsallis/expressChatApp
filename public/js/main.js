4f0e98860b6df508090251c3072edd57d770b7d3
silvertale <n.sallis@hotmail.com>
2015-01-11 23:46:27 -0500

    test

--- public/js/main.js
+++ public/js/main.js
@@ -21,10 +21,10 @@ if(document.getElementById("usertext").value != ""){
 
 $("#chatform").submit(function() {
 if(document.getElementById("usertext").value != ""){
-    	socket.emit('message sent', "|" + document.getElementById("username").value + "|" + document.getElementById('usertext').value);//send the text with the username surrounded by "|"
-    	document.getElementById('usertext').value = "";
-    	return false;
+	socket.emit('message sent', "|" + document.getElementById("username").value + "|" + document.getElementById('usertext').value);//send the text with the username surrounded by "|"
+	document.getElementById('usertext').value = "";
 	}
+	return false;
 });
 
 
